ae13e5f2af341c41dce499b43987965fb2775dfa
MellerPeller <melker.eriksson@skola.sala.se>
2024-09-11 09:49:13 +0200

    Add project files.

---
+++ Counter Class WinForms/Counter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter_Class_WinForms
+{
+    public class Counter
+    {
+        int count;
+
+        public Counter(int start)
+        {
+            count = start;
+
+        }
+
+
+
+        public void Increment()
+        {
+            count++;
+        }
+
+        public void Incrementby(int value)
+        {
+
+            count += value;
+
+        }
+
+
+        public void Reset()
+        {
+            count = 0;
+        }
+
+
+        public int GetValue()
+        {
+            return count;
+        }
+    }
+}
